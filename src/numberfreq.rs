42d407df136f95748c208f87f2e3b3f67aa9e667
nerdthatnoonelikes <kenrag24@gmail.com>
2020-11-22 13:59:33 -0600

    show key with highest value

--- src/numberfreq.rs
+++ src/numberfreq.rs
@@ -7,5 +7,5 @@ pub fn run() {
         let insert = map.entry(num).or_insert(0);
         *insert += 1;
     }
-    println!("{:?}", map);
+    println!("Number that appears the most times is {:?}", map.iter().max_by_key(|(_, &value)| value));
 }
